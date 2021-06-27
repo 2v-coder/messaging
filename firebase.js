48e02d1f63a1866ac7ccb04fc165482d13f6f23a
2v-coder <47473115+2v-coder@users.noreply.github.com>
2021-06-27 20:47:39 +0400

    Firebase Config

---
+++ firebase.js
@@ -0,0 +1,23 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID",
+};
+
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
